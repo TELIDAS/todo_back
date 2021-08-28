d290a8db8b4fad7c452215ac840448dad5fd67b7
telidas <TELIDAS>
2021-08-28 20:19:22 +0600

    setting changes

--- todo/admin.py
+++ todo/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from .models import ToDo
+
+from todo.models import TodoText
 
 
 class ToDoAdmin(admin.ModelAdmin):
-    list_display = ('title',
-                    'description',
-                    'completed')
+    list_display = ('username',
+                    'email',
+                    'text')
 
 
-admin.site.register(ToDo, ToDoAdmin)
+admin.site.register(TodoText, ToDoAdmin)
