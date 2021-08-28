b48ffec5f153e2f92838d0f0e07bb246d4b23efc
telidas <TELIDAS>
2021-08-28 21:51:57 +0600

    whitenoise

--- todo_test/settings.py
+++ todo_test/settings.py
@@ -127,8 +127,7 @@ USE_TZ = True
 # STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_HOST = os.environ.get('DJANGO_STATIC_HOST', '')
-STATIC_URL = STATIC_HOST + '/static/'
+STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
